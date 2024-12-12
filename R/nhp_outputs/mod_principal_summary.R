aed1039c80cbf7dc6c582479ae5595a30e53ee0c
fikriyudin11 <fikriyudin.mufasir@nhs.net>
2024-12-12 16:01:05 +0000

    renaming the function by adding inpatient to avoid any confusion

--- R/nhp_outputs/mod_principal_summary.R
+++ R/nhp_outputs/mod_principal_summary.R
@@ -1,4 +1,5 @@
-mod_principal_summary_data <- function(r, sites) {
+
+mod_principal_summary_data_inpatient <- function(r, sites) {
   pods <- mod_principal_los_pods()   # uses same POD lookup as LoS summary
 
   main_summary <- get_principal_high_level(
@@ -112,7 +113,6 @@ mod_principal_summary_data_outpatient <- function(r, sites) {
     dplyr::filter(activity_type == "Outpatient")
 }
 
-
 mod_principal_summary_data_ae <- function(r, sites) {
   pods <- mod_principal_los_pods()   # uses same POD lookup as LoS summary
 
@@ -167,59 +167,6 @@ mod_principal_summary_data_ae <- function(r, sites) {
     dplyr::filter(activity_type == "A&E")
 }
 
-mod_principal_summary_data_beds <- function(r, sites) {
-  pods <- mod_principal_los_pods()   # uses same POD lookup as LoS summary
-
-  main_summary <- get_principal_high_level(
-    r,
-    c("admissions", "attendances", "walk-in", "ambulance"),
-    sites
-  ) |>
-    dplyr::inner_join(pods, by = "pod")
-
-  bed_days <- get_principal_high_level(r, "beddays", sites) |>
-    dplyr::inner_join(pods, by = "pod") |>
-    dplyr::mutate(
-      "pod_name" = stringr::str_replace(.data$pod_name, "Admission", "Bed Days")
-    )
-
-  dplyr::bind_rows(main_summary, bed_days) |>
-    dplyr::mutate(
-      dplyr::across(
-        "activity_type",
-        ~ dplyr::case_match(
-          .data$activity_type,
-          "ip" ~ "Inpatient",
-          "op" ~ "Outpatient",
-          "aae" ~ "A&E",
-          .default = "Beds Available"
-        )
-      ),
-      dplyr::across(
-        "activity_type",
-        ~ factor(.x, levels = c("Inpatient", "Outpatient", "A&E", "Beds Available"))
-      ),
-      measure = dplyr::case_when(
-        stringr::str_detect(.data$pod_name, "Admission$") ~ "admission",
-        stringr::str_detect(.data$pod_name, "Attendance$") ~ "attendance",
-        stringr::str_detect(.data$pod_name, "Tele-attendance$") ~ "tele_attendance",
-        stringr::str_detect(.data$pod_name, "Procedure$") ~ "procedure",
-        stringr::str_detect(.data$pod_name, "Bed Days$") ~ "bed_days"
-      ),
-      change = .data$principal - .data$baseline,
-      change_pcnt = .data$change / .data$baseline
-    ) |>
-    dplyr::arrange(.data$activity_type, .data$measure, .data$pod_name) |>
-    dplyr::select(
-      "pod_name",
-      "activity_type",
-      "baseline",
-      "principal",
-      "change",
-      "change_pcnt"
-    ) |>
-    dplyr::filter(activity_type ==  "Beds Available")
-}
 
 mod_principal_summary_table <- function(data) {
   data |>
