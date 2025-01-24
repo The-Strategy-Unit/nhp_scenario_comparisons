2d4ac1d4e0d581b64d23dbd8885d272cc18fb516
Fikriyudin <105799693+Fikriyudin11@users.noreply.github.com>
2025-01-24 17:33:14 +0000

    Revert "Impact table"

--- R/nhp_outputs/mod_principal_change_factor_effects.R
+++ R/nhp_outputs/mod_principal_change_factor_effects.R
@@ -1,6 +1,4 @@
-#function to create waterfall chart
-
-mod_principal_change_factor_effects_summarised_ndg1 <- function(data, measure, include_baseline) {
+mod_principal_change_factor_effects_summarised <- function(data, measure, include_baseline) {
   data <- data |>
     dplyr::filter(
       .data$measure == .env$measure,
@@ -20,65 +18,6 @@ mod_principal_change_factor_effects_summarised_ndg1 <- function(data, measure, i
       )
     )
 
-  cfs <- data |>
-    dplyr::group_by(.data$change_factor) |>
-    dplyr::summarise(dplyr::across("value", \(.x) sum(.x, na.rm = TRUE))) |>
-    dplyr::mutate(cuvalue = cumsum(.data$value)) |>
-    dplyr::mutate(
-      hidden = tidyr::replace_na(dplyr::lag(.data$cuvalue) + pmin(.data$value, 0), 0),
-      colour = dplyr::case_when(
-        .data$change_factor == "Baseline" ~ "#f9bf07",
-        .data$value >= 0 ~ "#f9bf07",
-        TRUE ~ "#f9bf07"
-      ),
-      dplyr::across("value", abs)
-    ) |>
-    dplyr::select(-"cuvalue")
-
-  levels <- unique(c("baseline", levels(forcats::fct_drop(cfs$change_factor)), "Estimate"))
-  if (!include_baseline) {
-    levels <- levels[-1]
-  }
-
-  cfs |>
-    dplyr::bind_rows(
-      dplyr::tibble(
-        change_factor = "Estimate",
-        value = sum(data$value),
-        hidden = 0,
-        colour = "#f9bf07"
-      )
-    ) |>
-    tidyr::pivot_longer(c("value", "hidden")) |>
-    dplyr::mutate(
-      dplyr::across("colour", \(.x) ifelse(.data$name == "hidden", NA, .x)),
-      dplyr::across("name", \(.x) forcats::fct_relevel(.x, "hidden", "value")),
-      dplyr::across("change_factor", \(.x) factor(.x, rev(levels)))
-    )
-}
-
-
-
-mod_principal_change_factor_effects_summarised_ndg2 <- function(data, measure, include_baseline) {
-  data <- data |>
-    dplyr::filter(
-      .data$measure == .env$measure,
-      include_baseline | .data$change_factor != "baseline",
-      .data$value != 0
-    ) |>
-    tidyr::drop_na("value") |>
-    dplyr::mutate(
-      dplyr::across(
-        "change_factor",
-        \(.x) forcats::fct_reorder(.x, -.data$value)
-      ),
-      # baseline may now not be the first item, move it back to start
-      dplyr::across(
-        "change_factor",
-        \(.x) forcats::fct_relevel(.x, "baseline")
-      )
-    )
-  
   cfs <- data |>
     dplyr::group_by(.data$change_factor) |>
     dplyr::summarise(dplyr::across("value", \(.x) sum(.x, na.rm = TRUE))) |>
@@ -87,25 +26,25 @@ mod_principal_change_factor_effects_summarised_ndg2 <- function(data, measure, i
       hidden = tidyr::replace_na(dplyr::lag(.data$cuvalue) + pmin(.data$value, 0), 0),
       colour = dplyr::case_when(
         .data$change_factor == "Baseline" ~ "#686f73",
-        .data$value >= 0 ~ "#686f73",
-        TRUE ~ "#686f73"
+        .data$value >= 0 ~ "#f9bf07",
+        TRUE ~ "#2c2825"
       ),
       dplyr::across("value", abs)
     ) |>
     dplyr::select(-"cuvalue")
-  
+
   levels <- unique(c("baseline", levels(forcats::fct_drop(cfs$change_factor)), "Estimate"))
   if (!include_baseline) {
     levels <- levels[-1]
   }
-  
+
   cfs |>
     dplyr::bind_rows(
       dplyr::tibble(
         change_factor = "Estimate",
         value = sum(data$value),
         hidden = 0,
-        colour = "#686f73"
+        colour = "#ec6555"
       )
     ) |>
     tidyr::pivot_longer(c("value", "hidden")) |>
@@ -115,7 +54,6 @@ mod_principal_change_factor_effects_summarised_ndg2 <- function(data, measure, i
       dplyr::across("change_factor", \(.x) factor(.x, rev(levels)))
     )
 }
-
 
 mod_principal_change_factor_effects_cf_plot <- function(data) {
   data |>
@@ -151,7 +89,7 @@ mod_principal_change_factor_effects_cf_plot <- function(data) {
       labels = scales::comma
     ) +
     ggplot2::scale_y_discrete(labels = snakecase::to_title_case) +
-    ggplot2::labs(x = "", y = "") + ggplot2::facet_grid(rows = vars(scenario)) 
+    ggplot2::labs(x = "", y = "")
 }
 
 mod_principal_change_factor_effects_ind_plot <- function(data, change_factor, colour, title, x_axis_label) {
