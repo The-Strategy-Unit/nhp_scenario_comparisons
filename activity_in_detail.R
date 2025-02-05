02d4a0c3dd7562321d908279c382df7a77fd054c
HOBRO <gabriel.hobro@nhs.net>
2025-02-05 16:51:31 +0000

    example of using `generate_activity_in_detail_table` function

---
+++ activity_in_detail.R
@@ -0,0 +1,39 @@
+library(jsonlite)
+library(tidyverse)
+library(dplyr)
+library(gt)
+library(here)
+library(ggplot2)
+library(ggeasy)
+
+file_names <- list.files(path = 'R/nhp_outputs', pattern = "\\.R$")
+lapply(paste0('R/nhp_outputs/',file_names), source)
+
+# read Json ndg1
+path_ndg1 <- "jsons/Imperialv1-ndg1-20241022_100916.json.gz"
+
+result_ndg1 <- path_ndg1 |>
+  jsonlite::read_json() |>
+  parse_results()  # will apply necessary patches to the data
+
+# generating ip elective admissions by treatment specialty
+generate_activity_in_detail_table(
+  data = result_ndg1,
+  sites = NULL,
+  #tretspefs = "tretspefs",
+  activity_type = "ip",
+  pod = "ip_elective_admission",
+  measure = "admissions",
+  agg_col ="tretspef")
+
+# generating outpatient appointments by age group[]
+generate_activity_in_detail_table(
+  data = result_ndg1,
+  sites = NULL,
+  #tretspefs = "tretspefs",
+  activity_type = "op",
+  pod = "op_first",
+  measure = "attendances",
+  agg_col ="age_group")
+
+
