c629fe365ac857d46ff9840324c5ef5c7eb98755
Gabriel Hobro <gabriel.hobro@nhs.net>
2025-08-08 16:27:23 +0100

    added deploy.R script

---
+++ deploy.R
@@ -0,0 +1,9 @@
+rsconnect::deployApp(
+  appFiles = c("R/", "scenario_analysis_summary.qmd", "app.R", "load-data.R", "R/nhp_outputs/"),
+  appName = "scenario-comparison-app",
+  appTitle = "Scenario comparisons app prototype",
+  server = "connect.strategyunitwm.nhs.uk",
+  appId = 185,
+  lint = FALSE,
+  forceUpdate = TRUE
+)
