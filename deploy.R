f5d06707b7169b03ef41d25c4106dae944659aa3
Gabriel Hobro <gabriel.hobro@nhs.net>
2025-08-12 18:11:18 +0100

    "supporting_data" is spelt with an underscore, not a hyphen

--- deploy.R
+++ deploy.R
@@ -1,5 +1,5 @@
 rsconnect::deployApp(
-  appFiles = c("R/", "scenario_analysis_summary.qmd", "app.R", "load-data.R", "R/nhp_outputs/", "supporting-data/"),
+  appFiles = c("R/", "scenario_analysis_summary.qmd", "app.R", "load-data.R", "R/nhp_outputs/", "supporting_data/"),
   appName = "scenario-comparison-app",
   appTitle = "Scenario comparisons app prototype",
   server = "connect.strategyunitwm.nhs.uk",
