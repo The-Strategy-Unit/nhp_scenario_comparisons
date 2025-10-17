c5b14e917a38da1a48d5d520adbef81a7d936c75
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2025-10-17 13:23:26 +0100

    moves ui code into ui.R file

--- R/ui.R
+++ R/ui.R
@@ -2,6 +2,30 @@
 #'
 #' @param request Internal parameter for `{shiny}`.
 #' @noRd
-app_ui = function(request) {
+#' 
 
+nhp_model_runs <- get_nhp_result_sets() |> 
+  dplyr::filter(!app_version == "dev")
+
+app_ui = function(request) {
+  
+  bslib::page_sidebar(
+    shinybusy::add_busy_spinner(position = "bottom-right"),
+    title = "Scenario comparison app (proto)",
+    sidebar = bslib::sidebar(
+      title = "Scenario selection",
+      shiny::selectInput("selected_scheme", "Select scheme", choices = unique(nhp_model_runs$dataset)),
+      shiny::selectInput("scenario_1", "Select Scenario 1", choices = NULL),
+      shiny::selectInput("scenario_1_runtime", "Scenario 1 runtime", choices = NULL),
+      shiny::selectInput("scenario_2", "Select Scenario 2", choices = NULL),
+      shiny::selectInput("scenario_2_runtime", "Scenario 2 runtime", choices = NULL),
+      shiny::actionButton("render_quarto", "Render Quarto Summary"),
+      shiny::uiOutput("warning_text")
+    ),
+    bslib::card(
+      bslib::card_header("Result"),
+      shiny::textOutput("result_text"),
+      shiny::uiOutput("quarto_summary")
+    )
+  )
 }
