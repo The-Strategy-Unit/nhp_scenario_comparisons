05458ef9d834210ef9faeb106d1b34bd150abd83
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2025-10-20 12:48:21 +0100

    Adds create_login() to app.R

--- app.R
+++ app.R
@@ -1,2 +1,9 @@
+AzureRMR::create_azure_login()
+app <- AzureRMR::get_azure_login()$token$client$client_id
+AzureAuth::get_azure_token(
+  resource = "https://storage.azure.com/",
+  tenant = "common",
+  app = app
+)
 pkgload::load_all(".")
 run_app()
