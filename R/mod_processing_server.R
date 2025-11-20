6286b2614b6f750e571763aad0dd6e19f5d4b8ad
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2025-11-20 16:24:25 +0000

    fixes module function name

--- R/mod_processing_server.R
+++ R/mod_processing_server.R
@@ -1,4 +1,4 @@
-mod_summary_server <- function(id, scenario_selections, trigger){
+mod_processing_server <- function(id, scenario_selections, trigger){
   shiny::moduleServer(id, function(input, output, session){
     
     processed <- shiny::eventReactive(trigger()). {
