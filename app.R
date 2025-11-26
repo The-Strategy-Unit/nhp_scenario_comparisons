63714fd8615158061520bf4938f4892cdc5ff0da
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2025-11-26 00:38:06 +0000

    Potential fix for azure authenticate issue.

--- app.R
+++ app.R
@@ -1,9 +1,5 @@
-AzureRMR::create_azure_login()
-app <- AzureRMR::get_azure_login()$token$client$client_id
-AzureAuth::get_azure_token(
-  resource = "https://storage.azure.com/",
-  tenant = "common",
-  app = app
-)
+#AzureRMR::create_azure_login()
+#app <- AzureRMR::get_azure_login()$token$client$client_id
+azkit::get_auth_token()
 pkgload::load_all(".")
 run_app()
