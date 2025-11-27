0892bfb52c0c5208c3afe2796cea9a1d87736cf6
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2025-11-27 19:34:15 +0000

    fix factor levels in los bar plot

--- R/generate-bar.R
+++ R/generate-bar.R
@@ -12,7 +12,7 @@ create_bar_plot <- function(data, chosen_activity_type, chosen_measure, title_te
     ggplot2::ylab("Point of delivery") +
     ggplot2::xlab(chosen_measure) +
     ggplot2::scale_fill_manual(values = c("#f9bf07", "#686f73"), name = "Scenario"#, labels = c(scenario_1_name, scenario_2_name)
-                               ) +
+    ) +
     ggeasy::easy_center_title() +
     ggplot2::theme(text = element_text(family = "Segoe UI")) +
     ggplot2::theme(axis.text.x = element_text(family = "Segoe UI", size = 12, color = "black")) +
@@ -26,7 +26,10 @@ create_bar_plot <- function(data, chosen_activity_type, chosen_measure, title_te
 
 create_bar_plot_los <- function(data, chosen_pod_name, chosen_measure, title_text = "Example") {
   ggplot2::ggplot(
-    dplyr::filter(data, pod_name == chosen_pod_name, measure == chosen_measure),
+    dplyr::filter(data, pod_name == chosen_pod_name, measure == chosen_measure) |> 
+      dplyr::mutate(los_group = factor(los_group,
+                                       levels = los_group[order(as.numeric(str_extract(unique(los_group), "^\\d+")))])
+      ),
     ggplot2::aes(x = principal, y = forcats::fct_rev(los_group), fill = scenario)
   ) +
     ggplot2::geom_col(position = "dodge") +
@@ -35,7 +38,7 @@ create_bar_plot_los <- function(data, chosen_pod_name, chosen_measure, title_tex
     ggplot2::ylab("Length of stay group") +
     ggplot2::xlab(chosen_measure) +
     ggplot2::scale_fill_manual(values = c("#f9bf07", "#686f73"), name = "Scenario", #labels = c(scenario_1_name, scenario_2_name)
-                               ) +
+    ) +
     ggeasy::easy_center_title() +
     ggplot2::theme(text = element_text(family = "Segoe UI")) +
     ggplot2::theme(axis.text.x = element_text(family = "Segoe UI", size = 12, color = "black")) +
