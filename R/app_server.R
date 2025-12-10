ad209c3a49c60f59b63bcc330abb0ff54da1a770
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2025-12-10 23:43:04 +0000

    potential merge conflict fix

--- R/app_server.R
+++ R/app_server.R
@@ -7,8 +7,12 @@ lapply(paste0('R/nhp_outputs/',file_names_nhs_output), source)
 
 #this should be commented out in live versions
 
-#nhp_model_runs <- readRDS("inst/app/tmp_runs_file.rds") |> #tmp_runs_file.rds is an rds of the output of get_nhp_result_sets()
-# dplyr::filter(!app_version == "dev")
+# load_local_data <- TRUE
+# nhp_model_runs <- readRDS("inst/app/tmp_runs_file.rds") |> #tmp_runs_file.rds is an rds of the output of get_nhp_result_sets()
+#   dplyr::filter(!app_version == "dev") |> 
+#   dplyr::filter(stringr::str_extract(file, "[^/]+$") %in% 
+#                   list.files("jsons/")
+#   )
 
 app_server = function(input, output, session) {
   nhp_model_runs <- get_nhp_result_sets() |>
