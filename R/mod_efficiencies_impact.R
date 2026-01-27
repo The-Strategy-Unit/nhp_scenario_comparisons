7ef4b6c720da384cc9e3c82342c36acc53067c57
Ozayr Mohammed <ozayr.mohammed@nhs.net>
2026-01-27 15:06:55 +0000

    sets plot height

--- R/mod_efficiencies_impact.R
+++ R/mod_efficiencies_impact.R
@@ -7,7 +7,7 @@ mod_efficiencies_impact_ui <- function(id) {
     "Regard these results as rough, high-level estimates of the number of rows added/removed due to each parameter.",
     style = "margin-top: 15px; margin-bottom: 15px;"),
     shiny::uiOutput(ns("filters_ui")),
-    shiny::plotOutput(ns("plot"))
+    shiny::plotOutput(ns("plot"), height = "800px")
   )
 }
 
