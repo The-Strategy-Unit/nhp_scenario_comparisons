914a40fd4009b7b2269888faf414321a00cc8cad
HOBRO <gabriel.hobro@nhs.net>
2026-06-18 12:20:15 +0100

    get url directly from ATS (deprecating the need for the encrypt function)

--- R/model_run_metadata.R
+++ R/model_run_metadata.R
@@ -1,11 +1,6 @@
 get_metadata <- function(result_sets, scenario) {
   metadata <- scenario
 
-  # Generate encrypted bit of the outputs app URL
-  metadata$url_file_encrypted <- metadata$file |>
-    purrr::map(encrypt_filename) |>
-    unlist()
-
   metadata |>
     dplyr::mutate(
       create_datetime = create_datetime |>
@@ -16,13 +11,12 @@ get_metadata <- function(result_sets, scenario) {
       url_stub = glue::glue(
         "https://connect.strategyunitwm.nhs.uk/nhp/{url_app_version}/outputs/?"
       ),
-      outputs_link = glue::glue("{url_stub}{url_file_encrypted}")
+      outputs_link = glue::glue("{url_stub}{outputs_app_uri}")
     ) |>
     dplyr::mutate(
       outputs_app = glue::glue(
         "<a href='{outputs_link}' target='_blank'>Launch</a> \U1F517"
-      ),
-      .before = outputs_link
+      )
     ) |>
     dplyr::select(
       user,
@@ -36,21 +30,4 @@ get_metadata <- function(result_sets, scenario) {
       create_datetime,
       outputs_app
     )
-}
-
-encrypt_filename <- function(
-  filename,
-  key_b64 = Sys.getenv("NHP_ENCRYPT_KEY")
-) {
-  key <- openssl::base64_decode(key_b64)
-
-  f <- charToRaw(filename)
-
-  ct <- openssl::aes_cbc_encrypt(f, key, NULL)
-  hm <- as.raw(openssl::sha256(ct, key))
-
-  openssl::base64_encode(c(hm, ct)) |>
-    # Connect does something weird if it encounters strings of the form /w==,
-    # where / can be any special character.
-    URLencode(reserved = TRUE)
 }
