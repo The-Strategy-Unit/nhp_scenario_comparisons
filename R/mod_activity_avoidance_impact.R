1539c13b446f29389599ef2848362578c5f11e39
HOBRO <gabriel.hobro@nhs.net>
2026-06-22 16:36:13 +0100

    consolidate `shiny::req` functions

--- R/mod_activity_avoidance_impact.R
+++ R/mod_activity_avoidance_impact.R
@@ -54,12 +54,12 @@ mod_activity_avoidance_impact_server <- function(id, processed) {
 
     output$plot <- shiny::renderPlot(
       {
-        #shiny::req(df())
+        shiny::req(df(), input$filter1, input$filter2)
+        
         shiny::validate(
           shiny::need(!is.null(df()), message = "No data available"),
           shiny::need(nrow(df()) > 0, message = "No data available")
         )
-        shiny::req(input$filter1, input$filter2)
 
         impact_bar_plot(
           df(),
