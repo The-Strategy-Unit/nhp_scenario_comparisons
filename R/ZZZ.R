cc050509c6ec57242a8e1f097d6f5d856b1b2b00
Lextuga007 <zoe.turner3@nhs.net>
2026-07-08 10:21:57 +0100

    Added imports (from NHP Output as well as build checks)

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -1,5 +1,7 @@
 # This code originated from nhp_outputs
 #' @importFrom zeallot %<-%
+#' @importFrom rlang .data .env
+#' @importFrom stats quantile
 NULL
 
 require_rows <- function(x) {
@@ -7,3 +9,10 @@ require_rows <- function(x) {
   shiny::req(nrow(x) > 0)
   x
 }
+
+utils::globalVariables(c(
+  "where", # source: https://github.com/r-lib/tidyselect/issues/201#issuecomment-650547846
+  "ds",
+  "sc",
+  "cd" # because of the use of %<-%
+))
