07fde6571accb1c05c6decd77cc277f340682de9
Lextuga007 <zoe.turner3@nhs.net>
2026-07-08 11:22:16 +0100

    Added global variable .data and namespace for model_run_metadata.R

--- R/model_run_metadata.R
+++ R/model_run_metadata.R
@@ -3,11 +3,11 @@ get_metadata <- function(result_sets, scenario) {
 
   metadata |>
     dplyr::mutate(
-      create_datetime = create_datetime |>
+      create_datetime = .data$create_datetime |>
         lubridate::as_datetime() |>
         format("%Y-%m-%d %H:%M:%S") |>
         as.character(),
-      url_app_version = stringr::str_replace(app_version, "\\.", "-"),
+      url_app_version = stringr::str_replace(.data$app_version, "\\.", "-"),
       url_stub = glue::glue(
         "https://connect.strategyunitwm.nhs.uk/nhp/{url_app_version}/outputs/?"
       ),
@@ -19,15 +19,15 @@ get_metadata <- function(result_sets, scenario) {
       )
     ) |>
     dplyr::select(
-      user,
-      dataset,
-      scenario,
-      seed,
-      model_runs,
-      start_year,
-      end_year,
-      app_version,
-      create_datetime,
-      outputs_app
+      .data$user,
+      .data$dataset,
+      .data$scenario,
+      .data$seed,
+      .data$model_runs,
+      .data$start_year,
+      .data$end_year,
+      .data$app_version,
+      .data$create_datetime,
+      .data$outputs_app
     )
 }
